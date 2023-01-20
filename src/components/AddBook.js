3f2357317ae91ee4c7feacfb690eacc53d4e1441
AlejandraKeber <alejandrakeber@gmail.com>
2023-01-19 20:53:26 -0500

    Fix lintern error

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const AddBook = () => (
   <form>
-    <h2>Add new Book</h2>
+    <h2>ADD NEW BOOK</h2>
     <div className="form-container">
       <input type="text" className="title" name="title" placeholder="Book Title" />
       <input type="text" className="author" name="author" placeholder="Author" />
-      <button type="submit">Add Book</button>
+      <button type="submit">ADD BOOK</button>
     </div>
   </form>
 );
--- src/components/Book.js
+++ src/components/Book.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Book = (props) => {
+  Book.propTypes = {
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+  };
+
   const { title, author } = props;
+
   return (
     <div className="book-container">
       <h2>{title}</h2>
