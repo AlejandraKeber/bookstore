f2c39d8682a944390e79fdc7b73ec59c7aa5c06a
AlejandraKeber <alejandrakeber@gmail.com>
2023-01-24 17:53:16 -0500

    Add remove book functionality with useDispatch

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -3,7 +3,8 @@ const BOOK_REMOVED = 'bookstore/books/BOOK_REMOVED';
 
 const initialState = [
   { title: 'The Hunger Games', author: 'Suzanne Collins', id: '1' },
-  { title: 'Naked Lunch', author: 'William S. Burroughs', id: '2' },
+  { title: 'Dune', author: 'Frank Herbert', id: '2' },
+  { title: 'Capital in the Twenty-First Century', author: 'Suzanne Collins', id: '3' },
 ];
 
 const booksReducer = (state = initialState, action) => {
