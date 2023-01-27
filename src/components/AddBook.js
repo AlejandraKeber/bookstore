fcd369722cfd542774d792cd4d6e62899550ac16
AlejandraKeber <alejandrakeber@gmail.com>
2023-01-26 21:48:25 -0500

    Add styles to AddBook

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -2,27 +2,35 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import { addBook } from '../redux/books/books';
+import './AddBook.css';
 
 const AddBook = () => {
   const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const [title, author] = e.target;
-    dispatch(addBook({ title: title.value, author: author.value, item_id: uuidv4() }));
+    const [title, author, category] = e.target;
+    dispatch(addBook({
+      title: title.value,
+      author: author.value,
+      category: category.value,
+      item_id: uuidv4(),
+    }));
     title.value = '';
     author.value = '';
+    category.value = '';
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <div className="add-book-container">
       <h2>ADD NEW BOOK</h2>
-      <div className="form-container">
-        <input type="text" className="title" name="title" placeholder="Book Title" />
-        <input type="text" className="author" name="author" placeholder="Author" />
+      <form onSubmit={handleSubmit} className="add-form">
+        <input type="text" className="title" name="title" placeholder="Book Title" required />
+        <input type="text" className="author" name="author" placeholder="Author" required />
+        <input type="text" className="category" name="category" placeholder="Category" required />
         <button type="submit">ADD BOOK</button>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 };
 
